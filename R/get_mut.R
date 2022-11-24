6c1b8d98923ae6071380e24d7ce6f9585830d98a
Anh Phuong Le <al35@mib117980s.internal.sanger.ac.uk>
2022-11-24 10:29:06 +0000

    ENH: added get_context to get context from mutation files downloaded from icgc

--- R/get_mut.R
+++ R/get_mut.R
@@ -12,3 +12,32 @@ get_mut = function(kmer) {
   tail_mer = strsplit(kmer,"[", fixed = T)[[1]][2]
   return(strsplit(tail_mer,"]", fixed = T)[[1]][1])
 }
+
+#' get the relevant sequence & a kmer size and output a function that gets the context. This output function will be used in the apply function along a dataframe
+#' Note that input to the get_context function should be of the following orders: donor_id, chromosome, chrom_start, chrom_end, reference_genome_allele, mutated_from_base, mutated_to_base
+#'
+#' @param seq a vector of characters representing a DNA sequence)
+#' @param k an odd number greater than 1, default to 3
+#' @return a function to be applied to a dataframe, when applying the resulting function to a relevant dataframe, it returns a vector of mutations and their contexts
+#' @export
+#'
+#' @examples
+#' mut_dt = data.frame(donor_id = c('PD1','PD2'), chromosome = c('3', 'X'), chrom_start = c(5,7), chrom_end = c(5,7), reference_genome_allele = c('A','C'), mutated_from_base = c('A','C'), mutated_to_base = c('T', 'A'))
+#' seq = seqinr::s2c('AGCTAGCTGA')
+#' get_context = get_context_param(seq, k = 3)
+#' apply(mut_dt, MARGIN = 1, get_context)
+#'
+get_context_param = function(seq, k = 3) {
+  # k is the kmer size
+  if(k %% 2 != 1 | k <= 1) stop("k should be an odd number greater than 1")
+
+  get_context = function(x) {
+    pos = as.numeric(x[3])
+    if (x[5] != seq[pos]) stop("reference base from mutation dataframe should be the same as in the genome assembly")
+    if (x[3] != x[4]) stop("sbs should have the same chrom_start and chrom_end")
+    flank = k %/% 2
+    result = paste0(seq[(pos-flank):(pos-1)], "[", x[6], ">", x[7], "]", seq[(pos+1):(pos+flank)])
+    return(result)
+  }
+  return(get_context)
+}
--- tests/testthat/test-get_mut.R
+++ tests/testthat/test-get_mut.R
@@ -1,3 +1,55 @@
 test_that("get_mut() gets the central mutation", {
   expect_equal(get_mut('A[C>T]G'), 'C>T')
 })
+
+
+test_that("get_context_param() works", {
+  mut_dt = data.frame(donor_id = c('PD1','PD2'),
+                      chromosome = c('3', 'X'),
+                      chrom_start = c(5,7),
+                      chrom_end = c(5,7),
+                      reference_genome_allele = c('A','C'),
+                      mutated_from_base = c('A','C'), mutated_to_base = c('T', 'A'))
+  seq = seqinr::s2c('AGCTAGCTGA')
+  get_context = get_context_param(seq, k = 3) # seems to work hehe
+  result = apply(mut_dt, MARGIN = 1, get_context)
+  expected = c('T[A>T]G', 'G[C>A]T')
+  expect_equal(result, expected)
+})
+
+test_that("get_context_param() even number error", {
+  mut_dt = data.frame(donor_id = c('PD1','PD2'),
+                      chromosome = c('3', 'X'),
+                      chrom_start = c(5,7),
+                      chrom_end = c(5,7),
+                      reference_genome_allele = c('A','C'),
+                      mutated_from_base = c('A','C'), mutated_to_base = c('T', 'A'))
+  seq = seqinr::s2c('AGCTAGCTGA')
+  expect_error(get_context_param(seq, k = 4), "k should be an odd number greater than 1", fixed = T)
+  expect_error(get_context_param(seq, k = 12), "k should be an odd number greater than 1", fixed = T)
+})
+
+test_that("get_context_param() mut_dt differs from reference genome", {
+  mut_dt = data.frame(donor_id = c('PD1','PD2'),
+                      chromosome = c('3', 'X'),
+                      chrom_start = c(5,7),
+                      chrom_end = c(5,7),
+                      reference_genome_allele = c('G','C'),
+                      mutated_from_base = c('A','C'), mutated_to_base = c('T', 'A'))
+  seq = seqinr::s2c('AGCTAGCTGA')
+  get_context = get_context_param(seq, k = 3)
+  expect_error(apply(mut_dt, MARGIN = 1, get_context), "reference base from mutation dataframe should be the same as in the genome assembly", fixed = T)
+})
+
+test_that("get_context_param() mut_dt chrom_start differs from chrom_end", {
+  mut_dt = data.frame(donor_id = c('PD1','PD2'),
+                      chromosome = c('3', 'X'),
+                      chrom_start = c(5,7),
+                      chrom_end = c(8,7),
+                      reference_genome_allele = c('A','C'),
+                      mutated_from_base = c('A','C'), mutated_to_base = c('T', 'A'))
+  seq = seqinr::s2c('AGCTAGCTGA')
+  get_context = get_context_param(seq, k = 3)
+  expect_error(apply(mut_dt, MARGIN = 1, get_context), "sbs should have the same chrom_start and chrom_end", fixed = T)
+})
+
